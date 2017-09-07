c1a75ff5a40ce754feb84a1aea1575ed3da71935
Carlos Alberto Costa Beppler <beppler@gmail.com>
2017-09-07 02:36:44 -0300

    Adjust version information.

--- TrayLeds/Properties/AssemblyInfo.cs
+++ TrayLeds/Properties/AssemblyInfo.cs
@@ -7,7 +7,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("TrayLeds")]
 [assembly: AssemblyDescription("Tray Notification Leds")]
 [assembly: AssemblyProduct("TrayLeds")]
-[assembly: AssemblyCopyright("Copyright © Carlos Alberto Costa Beppler 2017")]
+[assembly: AssemblyCopyright("Copyright © 2017 Carlos Alberto Costa Beppler")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
@@ -27,5 +27,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.3.0.0")]
-[assembly: AssemblyFileVersion("0.3.0.0")]
+[assembly: AssemblyVersion("0.3.1.0")]
+[assembly: AssemblyFileVersion("0.3.1.0")]
